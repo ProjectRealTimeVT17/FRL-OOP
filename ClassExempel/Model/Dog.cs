393bf93e5b54567ec07b54e63f30f463db24bdef
John <John.hellman@lexicon.se>
2017-03-15 16:58:17 +0100

    Bobbys codealong

---
+++ ClassExempel/Model/Dog.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace ClassExempel.Model
+{
+    class Dog
+    {
+        public Dog(string name, string color, Gender gender, int age)
+        {
+            _name = name;
+            Color = color;
+            _gender = gender;
+            _dateOfBirth = DateTime.Now.AddYears((-1 * age));
+        }
+
+        #region Properties
+        private DateTime _dateOfBirth;
+        public int DateofBirth
+        {
+            get
+            {
+                return _dateOfBirth.Year;
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        private Gender _gender;
+        public Gender Gender
+        {
+            get
+            {
+                return _gender;
+            }
+        }
+
+        //public string Color { get; set; }
+        private string _color;
+        public string Color
+        {
+            get
+            {
+                return _color;
+            }
+
+            private set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _color = value;
+                }
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                _age = (DateTime.Now.AddYears(1).Year - _dateOfBirth.Year);
+                return _age;
+            }
+
+        }
+        #endregion
+
+
+        public string Bark()
+        {
+            string bark = "Woff!";
+            SpeechSynthesizer synth = new SpeechSynthesizer() { };
+            //synth.SelectVoiceByHints(VoiceGender.Female);
+            synth.Speak(bark);
+            return bark;
+        }
+    }
+}
---
+++ ClassExempel/Program.cs
@@ -0,0 +1,49 @@
+﻿using ClassExempel.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExempel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dog myDog = new Dog();
+            //myDog.Name = "Fido";
+            //myDog.Gender = Gender.Male;
+            //myDog.Age = 7;
+
+            //Dog myDog = new Dog()
+            //{
+            //    Name = "Fido",
+            //    Gender = Gender.Male,
+            //    SetAge = 7,
+            //    Color = null
+            //};
+
+            Dog myDog = new Dog("Fido", "Brown", Gender.Male, 7);
+            //Visar att property bara accepterar värdet "Brown" som är en sträng som inte är null, empty eller white-space
+            //myDog.Color = "Brown";
+            //myDog.Color = "";
+            //myDog.Color = " ";
+
+
+
+
+            Console.WriteLine(myDog.Bark());
+            Console.WriteLine((int)Gender.Male);
+            Console.WriteLine("My dogs name is: " + myDog.Name);
+            Console.WriteLine("My dogs gender is: " + myDog.Gender);
+            Console.WriteLine("My dogs age is: " + myDog.Age);
+            Console.WriteLine("My date of birth is: " + myDog.DateofBirth);
+            Console.WriteLine("My dogs color is: " + myDog.Color);
+
+            Cat myCat = new Cat();
+
+            Console.ReadKey();
+        }
+    }
+}
